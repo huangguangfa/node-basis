2911adf05769ea9a513f89dd7a46808142bc5e24
hgf <1454556135@qq.com>
2021-01-24 17:12:55 +0800

    添加保存图片

--- src/fs/fs.writeFile.js
+++ src/fs/fs.writeFile.js
@@ -2,10 +2,19 @@
 
 const fs = require('fs');
 
-const text = '这是要写入的文字内容';
+// const text = '这是要写入的文字内容';
 
-fs.writeFile(__dirname + '/edit1.text',text,(err,db) =>{
-    if(err === null){
-        console.log('写入成功！')
-    }
+// fs.writeFile(__dirname + '/edit1.text',text,(err,db) =>{
+//     if(err === null){
+//         console.log('写入成功！')
+//     }
+// })
+
+//写入图片进行保存
+fs.readFile('../img/img.jpg',(err,data) =>{
+    if (err) throw err;
+    fs.writeFile('../img/img1.png',data,(err,db) =>{
+        if (err) throw err;
+        console.log(db)
+    })
 })
